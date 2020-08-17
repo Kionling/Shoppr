de3a84c455ae1beb6512285405988d7c566aa619
Sham <shambhawi.kumari1393@gmail.com>
2020-08-17 15:21:30 -0700

    added routes for client side pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Search from "./pages/Search/Search";
+import Result from "./pages/Result/Result";
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,6 +15,10 @@ function App() {
           <Nav />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/result" component={Result} />
             {/* <Route component={NoMatch} /> */}
           </Switch>
       </div>
