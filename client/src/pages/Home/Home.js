13c90bde3029ad200cd9af81e5ba8934bafeaf0b
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-21 14:48:13 -0700

    Minor changes

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+
+const Styles= {
+    banner: {
+        
+    }
+}
+
+
 function Home(){
     return (
-        <div>
-            <h1>I am in Home Component</h1>
-        </div>
+        <div className="container center">
+            <div className="row" id="banner">
+                <img src={"https://i.pinimg.com/originals/15/06/13/150613c984bc132b04b7b723119ba63d.gif" }/>
+            </div>
+        </div> 
     );
 }
 
