b126d0f9d0709a1096d0f8708c36f660e8eb7a79
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-24 12:10:25 -0700

    Added banner to signup page

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -3,7 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { useShopprContext } from "../../utils/GlobalState";
 import Styles from "../Signup/signup.css";
 import API from "../../utils/API";
-
+import Atey from "../Signup/images/atey.jpg";
 function Signup() {
   let history = useHistory();
 
@@ -51,73 +51,73 @@ function Signup() {
     } // end of If Statement
   }
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col s12 l6">
+    <div className="">
+      <div className="row container">
+        <div className="col s12 l4 ">
           <h1 id="signup">Sign up With Shoppr:</h1>
         </div>
-          <div className="col s12 l4">
-            <form className="signup" onSubmit={handleSubmitForm}>
-              <div className="form-group">
-                <label>Username</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="username-input"
-                  placeholder="Username"
-                  ref={userNameRef}
-                />
-              </div>
-              <div className="form-group">
-                <label>Email address</label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="email-input"
-                  placeholder="Email"
-                  ref={emailRef}
-                />
-              </div>
-              <div className="form-group">
-                <label>Password</label>
-                <input
-                  type="password"
-                  className="form-control"
-                  id="password-input"
-                  placeholder="Password"
-                  ref={passwordRef}
-                />
-              </div>
-              <div
-                style={{ display: "none" }}
-                id="alert"
-                className="alert alert-danger"
-                role="alert"
-              >
-                <span
-                  className="glyphicon glyphicon-exclamation-sign"
-                  aria-hidden="true"
-                ></span>
-                <span className="sr-only">Error:</span>{" "}
-                <span className="msg"></span>
-              </div>
-              <button
-                type="submit"
-                className="btn  #00b0ff light-blue accent-3"
-              >
-                Sign Up
-              </button>
-            </form>
-            <br></br>
-            <p>
-              Or log in <Link to="/login">here</Link>
-            </p>
-          </div>
-        
+        <div className="col s12 l4">
+          <form className="signup" onSubmit={handleSubmitForm}>
+            <div className="form-group">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control"
+                id="username-input"
+                placeholder="Username"
+                ref={userNameRef}
+              />
+            </div>
+            <div className="form-group">
+              <label>Email address</label>
+              <input
+                type="email"
+                className="form-control"
+                id="email-input"
+                placeholder="Email"
+                ref={emailRef}
+              />
+            </div>
+            <div className="form-group">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control"
+                id="password-input"
+                placeholder="Password"
+                ref={passwordRef}
+              />
+            </div>
+            <div
+              style={{ display: "none" }}
+              id="alert"
+              className="alert alert-danger"
+              role="alert"
+            >
+              <span
+                className="glyphicon glyphicon-exclamation-sign"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Error:</span>{" "}
+              <span className="msg"></span>
+            </div>
+            <button type="submit" className="btn  #00b0ff light-blue accent-3">
+              Sign Up
+            </button>
+          </form>
+          <br></br>
+          <p>
+            Or log in <Link to="/login">here</Link>
+          </p>
+        </div>
       </div>
-      <div className="center">
+      <div id="signupbanner" className="center">
+        <div className="row center">
+          <div className="col s12 l12">
+            <h1 id="thankYou">Thank you for joining us here at Shoppr!</h1>
           </div>
-
+        </div>
+      </div>
     </div>
   );
 }
