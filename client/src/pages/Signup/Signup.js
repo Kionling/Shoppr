550bcb3c50761e14db80d8c7f9bd9f92786e991f
Kionlinggit config --global user.name KionlingKionlingKionling <djvdjv209@gmail.com>
2020-08-24 14:19:32 -0700

    Fixing banner styling

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -3,7 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { useShopprContext } from "../../utils/GlobalState";
 import Styles from "../Signup/signup.css";
 import API from "../../utils/API";
-import Atey from "../Signup/images/atey.jpg";
+import Atey from "../Signup/images/logoshort.png";
 function Signup() {
   let history = useHistory();
 
@@ -115,9 +115,54 @@ function Signup() {
         <div className="row center">
           <div className="col s12 l12">
             <h1 id="thankYou">Thank you for joining us here at Shoppr!</h1>
+            <img src={Atey}/>
           </div>
         </div>
       </div>
+      <footer class="page-footer #37474f blue-grey darken-3">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">Shoppr</h5>
+              <p class="grey-text text-lighten-4">
+                The latest shopping search engine.
+              </p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">The Team</h5>
+              <ul>
+                <li>
+                  <a
+                    class="grey-text text-lighten-3"
+                    href="https://github.com/shambhawi13"
+                  >
+                    Shambhawi
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="grey-text text-lighten-3"
+                    href="https://github.com/b0rgbart3"
+                  >
+                    Bart
+                  </a>
+                </li>
+                <li>
+                  <a
+                    class="grey-text text-lighten-3"
+                    href="https://github.com/Kionling"
+                  >
+                    Daniel
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">© 2020 Shoppr</div>
+        </div>
+      </footer>
     </div>
   );
 }
