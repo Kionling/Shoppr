2c73ea0b595f715b8d8cba59a7da4ae9346003f2
Bart Dority <b0rgBart3@gmail.com>
2020-08-26 18:59:56 -0700

    creating static rainforest_lamp

--- scripts/rainforest_static_search.js
+++ scripts/rainforest_static_search.js
@@ -6,7 +6,7 @@ let params = {
   api_key: "8AEA97E594DD4BCBBD393DB606E24152",
   type: "search",
   amazon_domain: "amazon.com",
-  search_term: "lamp",
+  search_term: "desk",
   sort_by: "price_high_to_low"
 }
 
