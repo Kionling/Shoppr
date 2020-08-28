b038f4f8db084b7a95a2eb3afce66a3eec9e44fc
Sham <shambhawi.kumari1393@gmail.com>
2020-08-28 13:27:33 -0700

    added cards to existing friend list

--- client/src/components/CurrentFriends/CurrentFriends.js
+++ client/src/components/CurrentFriends/CurrentFriends.js
@@ -1,27 +1,35 @@
-import React, {useRef, useEffect} from 'react';
+import React, { useRef, useEffect } from 'react';
 import "./CurrentFriends.css";
 import { useShopprContext } from "../../utils/GlobalState";
 import { SET_FRIENDS } from "../../utils/actions";
 import API from "../../utils/API";
+import user_avatar from "../../assets/user_avatar.png";
 
-function CurrentFriends(){
+function CurrentFriends() {
 
-const [state, dispatch] = useShopprContext();
+    const [state, dispatch] = useShopprContext();
 
-useEffect( ()=> {
-    console.log("In CurrentFriends, state.Friends:", state.Friends);
-});
+    useEffect(() => {
+        console.log("In CurrentFriends, state.Friends:", state.Friends);
+    });
 
-    return(
+    return (
         <div>
-          { (state.Friends && state.Friends.length > 0) ? state.Friends.map( (friend,index) => {
-              return (
-                  <div key={index}>
-                  
-                   <h5 id="friends" >{ friend.username }</h5>
-                  </div>
-              )
-          }) : <div><h1>You have not added any friends yet.     </h1> </div>}
+            {(state.Friends && state.Friends.length > 0) ? state.Friends.map((friend, index) => {
+                return (
+                        <div
+                            className="friendCard"
+                            key={index}
+                        >
+                            <img
+                                src={friend.avatar ? friend.avatar : user_avatar}
+                                className="avatar"
+                            />
+                            <h1 className="textWeight">{friend.username}</h1>
+                            <p>{friend.email}</p>
+                        </div>
+                )
+            }) : <div><h1>You have not added any friends yet.     </h1> </div>}
         </div>
     )
 
